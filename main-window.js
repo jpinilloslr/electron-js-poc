2341e5bef01c1e6f01933290684c14213ab496b3
Joaquin <jpinilloslr@gmail.com>
2018-04-22 20:22:27 -0300

    Add explore zip button.

--- main-window.js
+++ main-window.js
@@ -1,8 +1,8 @@
-'use strict';
-
-const {BrowserWindow} = require('electron');
+const {BrowserWindow, remote} = require('electron');
 const path = require('path');
 const url = require('url');
+const fs = require('fs');
+const DecompressZip = require('decompress-zip');
 
 let win;
 
@@ -22,4 +22,67 @@ function create() {
     }    
 }
 
+function uploadZipFile() {
+}
+
+function extractAndExploreZipFile() {
+    var fileNames = remote.dialog.showOpenDialog(win, {
+        properties: ['openFile'],
+        filters: [
+            { name: 'Zip file', extensions: ['zip'] },            
+            { name: 'All files', extensions: ['*'] }
+          ]
+      });
+
+      if(fileNames.length > 0) {
+        uncompress(fileNames[0], "extracted-data", listContent);
+      }
+}
+
+function uncompress(sourceFile, destinationPath, onDone){
+    var unzipper = new DecompressZip(sourceFile);
+
+    unzipper.on('error', function (err) {
+        console.log('Caught an error', err);
+    });
+    
+    unzipper.on('extract', onDone);
+    
+    unzipper.on('progress', function (fileIndex, fileCount) {
+        
+        console.log('Extracted file ' + (fileIndex + 1) + ' of ' + fileCount);
+    });
+    
+    unzipper.extract({
+        path: destinationPath
+    });
+}
+
+function println(text) {
+    document.getElementById('output').innerText += text + '\r\n';
+}
+
+function listContent() {
+    var fileList = [];
+    walkSync("extracted-data", fileList);
+    fileList.forEach(function(currentFile) {
+        println(currentFile);
+    });
+}
+
+function walkSync(dir, filelist) {
+    var files = fs.readdirSync(dir);
+    filelist = filelist || [];
+    files.forEach(function(file) {
+        if (fs.statSync(dir + '/' + file).isDirectory()) {
+            filelist = walkSync(dir + '/' + file, filelist);
+        }
+        else {
+            filelist.push(file);
+        }
+    });
+    return filelist;
+};
+
 exports.create = create;
+exports.uploadZipFile = uploadZipFile;
